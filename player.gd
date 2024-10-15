bacc3ee096a7abc8d1d32995329d5f6ab9260ec9
Mathias Siggaard Jørgensen <matjoerg.dk@gmail.com>
2024-10-15 10:33:10 +0200

    Add player movement

---
+++ player.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+@export var speed = 400
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	velocity = velocity.normalized() * speed
+	position += velocity * delta
+	
+	# Manual screen boundary check
+	position.x = clamp(position.x, 0, screen_size.x)
